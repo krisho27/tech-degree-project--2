b123e043fc9ad4c106550a797b685a1fcbcb66b3
krisho27 <krisshreiner@gmail.com>
2024-12-05 12:37:00 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const studentList = data;
 const linkList = document.getElementsByClassName('link-list')[0];
 const header = document.querySelector('.header');
+const searchBarHTML = `
+   <label for="search" class="student-search">
+      <span>Search by name</span>
+      <input id="search" placeholder="Search by Name...">
+      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+   </label>`
+header.insertAdjacentHTML('beforeend', searchBarHTML);
+
+const searchInput = document.getElementById('search');
+const searchButton = document.querySelector('.student-search button');
+const ul = document.getElementsByClassName('student-list')[0];
+const itemsOnPage = 9;
 
 
 /*
