06b13cb59c4ea24d65823aa2c11896625e4c0886
krisho27 <krisshreiner@gmail.com>
2024-12-05 16:37:51 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,78 +1,86 @@
-const studentList = data;
+const studentsList = data; //call data
 const linkList = document.getElementsByClassName('link-list')[0];
 const header = document.querySelector('.header');
 const searchBarHTML = `
-   <label for="search" class="student-search">
-      <span>Search by name</span>
-      <input id="search" placeholder="Search by Name...">
-      <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
-   </label>`
+  <label for="search" class="student-search">
+    <span>Search by name</span>
+    <input id="search" placeholder="Search by name...">
+    <button type="button"><img src="img/icn-search.svg" alt="Search icon"></button>
+  </label>`;
 header.insertAdjacentHTML('beforeend', searchBarHTML);
 
 const searchInput = document.getElementById('search');
 const searchButton = document.querySelector('.student-search button');
 const ul = document.getElementsByClassName('student-list')[0];
-const itemsOnPage = 9;
+const itemsPerPage = 9;
 
 
 
-//Search
+//search input
 searchInput.addEventListener('input', function() {
    const newData = [];
    const userInput = searchInput.value.toLowerCase();
 
-   for (let i = 0; i < studentList.length; i++) {
-      const studentsName = `${data[i].name.title} ${data[i].name.first} ${data[i].name.last}`.toLowerCase();
-      if (studentsName.includes(userInput)) {
-         newData.push(studentList[i]);
-      }
+   for (let i = 0; i < studentsList.length; i++) {
+      //need to seperate the names out since it's 3 fields
+      const studentName = `${data[i].name.title} ${data[i].name.first} ${data[i].name.last}`.toLowerCase();
+
+       if (studentName.includes(userInput)) {
+           newData.push(studentsList[i]);
+       }
    }
 
    if (newData.length > 0) {
-      handlePagination(newData);
-      showPage(newData, 1);
+       handlePagination(newData);
+       showPage(newData, 1);
    } else {
-      const html = "<h3>No Results Found...</h3>";
-      ul.innerHTML = html;
-      linkList.innerHTML = "";
+       const html = "<h3>No Results Found...</h3>";
+       ul.innerHTML = html;
+       linkList.innerHTML = "";
    }
 });
 
-//Handle Pagination
+// Handle pagination
 function handlePagination(list) {
-   const numberBtns = Math.ceil(list.length / itemsOnPage);
+   const numberButtons = Math.ceil(list.length / itemsPerPage);
    linkList.innerHTML = "";
 
-   for (let i = 1; i <= numberBtns; i++) {
-      const btnMarkup = `<li><button>${i}</button></li>`;
-      linkList.insertAdjacentHTML("beforeend", btnMarkup);
+   for (let i = 1; i <= numberButtons; i++) {
+       const buttonMarkup = `<li><button>${i}</button></li>`;
+       linkList.insertAdjacentHTML("beforeend", buttonMarkup);
    }
 
-   const firstBtn = linkList.querySelector("button");
-   if (firstBtn) firstBtn.classList.add("active");
+   const firstButton = linkList.querySelector("button");
+   if (firstButton) firstButton.classList.add("active");
 }
 
-//Create showPage function
-function showPage(list, page) {
-   const start = (page * itemsOnPage) - itemsOnPage;
-   const end = page * itemsOnPage;
 
+/*
+Create the `showPage` function
+This function will create and insert/append the elements needed to display a "page" of nine students
+*/
+function showPage(list, page) {
+
+   const start = (page * itemsPerPage) - itemsPerPage;
+   const end = page * itemsPerPage;
+
+  
    ul.innerHTML = "";
 
    for (let i = 0; i < list.length; i++) {
       if (i >= start && i < end) {
-         const studentsList = `
-         <li class="student-item cf">
-         <div class="student-details">
-            <img src=${list[i].picture.medium} alt="Profile Picture" class="avatar"`
-            <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
-            <span class="email">${list[i].email}</span>
-         </div>
-         <div class="joined-details">
-            <span class="date">Joined ${list[i].registered.date}</span>
-         </div>
-      </li>
-            ul.insertAdjacentHTML('beforeend', studentsList)
+         const studentList = `
+  <li class="student-item cf">
+  <div class="student-details">
+    <img src=${list[i].picture.medium}  alt="Profile Picture" class="avatar">
+    <h3>${list[i].name.title} ${list[i].name.first} ${list[i].name.last}</h3>
+    <span class="email">${list[i].email}</span>
+  </div>
+  <div class="joined-details">
+    <span class="date">Joined ${list[i].registered.date}</span>
+  </div>
+</li>`;
+         ul.insertAdjacentHTML('beforeend', studentList);
       }
    }
 };
